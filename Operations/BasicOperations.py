1b03ef02297da950588812ecb764468d3a0ffc54
Kenny H <kennyhuynh1198@gmail.com>
2018-09-04 19:46:17 +1000

    extended and fixed BasicOperations.py + added functionality to Calculation.py
    Calculation.py:
    - Fixed menu selector.
    - Added more options (add, sub, multi, div, expo)
    - Refined output (floats to 2dp).
    
    BasicOperations.py:
    - Added Addition, Subtraction, Multiplication, Division and Exponentiation functions.
    
    Note: No input/error handling utilised.
    No PyDoc/Design By Contract implementation.

--- Operations/BasicOperations.py
+++ Operations/BasicOperations.py
@@ -1,5 +1,52 @@
-def Addition(numList):
+def Addition():
+    numList = list()
     result = 0
+    average = 0
+    counter = 0
+    userInput = ""
+    while userInput != " ":
+        userInput = input("(Input whitespace if you want to continue) Enter a number(#{0}): ".format(counter + 1))
+        if userInput is not " ":
+            numList.append(int(userInput))
+            counter += 1
+
     for x in numList:
         result += x
+    average = result/counter
+    return result, average
+
+def Subtraction():
+    userInput = int(input("Enter the first number: "))
+    userInput2 = int(input("Enter the second number: "))
+    result = 0
+    result = userInput - userInput2
+    return result
+
+def Multiplication():
+    numList = list()
+    result = 1
+    counter = 0
+    userInput = ""
+    while userInput != " ":
+        userInput = input("(Input whitespace if you want to continue) Enter a number(#{0}): ".format(counter + 1))
+        if userInput is not " ":
+            numList.append(int(userInput))
+            counter += 1
+
+    for x in numList:
+        result *= x
+    return result
+
+def Division():
+    userInput = int(input("Enter the first number: "))
+    userInput2 = int(input("Enter the second number: "))
+    result = 0
+    result = userInput/userInput2
+    return result
+
+def Exponentiation():
+    userInput = int(input("Enter the base number: "))
+    userInput2 = int(input("Enter the exponent: "))
+    result = 0
+    result = userInput ** userInput2
     return result
--- Operations/Calculation.py
+++ Operations/Calculation.py
@@ -1,19 +1,33 @@
 import BasicOperations
+
 class Calculation():
 
-    numList = list()
     quit = False
+
     while quit != True:
         print("Operation select:")
-        print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exponentiation")
-        operation = int(input("Please select an operation: "))
+        print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exponentiation\n")
+        print("Press 'q' to quit.\n")
+        operation = input("Please select an operation: ")
 
-    
-        num  = int(input("Enter amount of numbers to evaluate: "))
+        result = 0
+        average = 0
 
-        for x in range(num):
-            numToAdd = int(input("Enter a number(#" + str(x) + "): "))
-            numList.append(numToAdd)
-
-        if operation == 1:
-            print(BasicOperations.Addition(numList))
+        if operation == '1':
+            result, average = BasicOperations.Addition()
+            print("\nAddition result: %d\nAverage: %.2f\n" % (result, average))
+        elif operation == '2':
+            result = BasicOperations.Subtraction()
+            print("\nSubtraction result: %d\n" % (result))
+        elif operation == '3':
+            result = BasicOperations.Multiplication()
+            print("\nMultiplication result: %d\n" % (result))
+        elif operation == '4':
+            result = BasicOperations.Division()
+            print("\nDivision result: %.2f\n" % (result))
+        elif operation == '5':
+            result = BasicOperations.Exponentiation()
+            print("\nExponentiation result: %d\n" % result)
+        elif operation == 'q':
+            print("\nTerminating process...")
+            quit = True
